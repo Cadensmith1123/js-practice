b3c774f1a00cb10a744d47abe1102e0961b77d99
Caden Smith <cadensmith1123@gmail.com>
2022-04-03 16:58:27 -0700

    initial

---
+++ script.js
@@ -0,0 +1,27 @@
+// // Fibonucci
+
+// let result, a, b;
+
+// a = 0;
+// b = 1;
+// result = b;
+
+// for (i = 1; i < 100; i++) {
+//     document.write(result + "<br/>")
+//     result = a + b;
+//     a = b;
+//     b = result;
+// }
+
+
+// let palindrome = function (word) {
+//     let len = word.length;
+//     let start = word.substring(0, Math.floor(len/2) ).toLowerCase();
+//     let end = word.substring(len - Math.floor(len/2)).toLowerCase();
+//     let flip = end.split('').reverse().join('');
+//     return (start === flip);
+// }
+
+// console.log(palindrome('radar'))
+// console.log(palindrome('redder'))
+// console.log(palindrome('window'))
