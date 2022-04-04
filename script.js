acc1e3871b9daceca38b8ac1c331c9a3369700ff
Caden Smith <cadensmith1123@gmail.com>
2022-04-03 18:57:22 -0700

    more js problems

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// // Fibonucci
+// // Fibonucci ----------------------------------------------------------------------
 
 // let result, a, b;
 
@@ -13,7 +13,7 @@
 //     b = result;
 // }
 
-
+// Palindrome ------------------------------------------------------------------------
 // let palindrome = function (word) {
 //     let len = word.length;
 //     let start = word.substring(0, Math.floor(len/2) ).toLowerCase();
@@ -25,3 +25,52 @@
 // console.log(palindrome('radar'))
 // console.log(palindrome('redder'))
 // console.log(palindrome('window'))
+
+
+
+//FizzBuzz -------------------------------------------------------------------------
+
+// for (i = 1; i < 101; i++) {
+//     if (i % 5 === 0 && i % 3 === 0) {
+//         console.log("FizzBuzz")
+//     } else if (i % 5 === 0) {
+//         console.log("Fizz")
+//     } else if (i % 3 === 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+//timeout -------------------------------------------------------------------------------------------
+//What order will it print?? and why?
+
+// console.log('a');
+// let timmy = setTimeout(function () {
+//     console.log('b')
+// }, 1);
+// let timothy = setTimeout(function () {
+//     console.log('c')
+// }, 10);
+// let timer = setTimeout(function () {
+//     console.log('d')
+// }, 0) 
+// console.log('e')
+
+
+// Async functions ---------------------------------------------------------------------------------
+
+// let num = 0;
+// async function increment() {
+//     num += await 2; // Promise doesnt run until end of main stack
+//     console.log(num)
+// }
+// increment();
+// num += 1;
+// console.log(num)
+
+// Which is the output?
+// 2, 3
+// 1, 3
+// 1, 2
+// 2, 1
